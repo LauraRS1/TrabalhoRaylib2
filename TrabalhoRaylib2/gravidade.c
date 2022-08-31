2d2989877bb213a8e27e04b48566b4a07dd06bee
Thiago Arend <thiago.arend@gmail.com>
2022-08-30 22:18:41 -0300

    implementando o dano de queda

--- TrabalhoRaylib2/gravidade.c
+++ TrabalhoRaylib2/gravidade.c
@@ -1,10 +1,22 @@
 
 #include "mapa.h"
+#include "stdio.h"
+#include "jogador.h"
 
 void gravidade(Mapa *mapa){
     mapa_localiza_jogador(mapa);
     char aux;
     aux = mapa->mapa[mapa->jogador.localizacao.linha + 1][mapa->jogador.localizacao.coluna];
+    char chao = mapa->mapa[mapa->jogador.localizacao.linha - 1][mapa->jogador.localizacao.coluna]; // char do conteudo abaixo do jogador
+
+    if((chao == 'X') || (chao == 'H')) { // se jogador alcancar o chao
+        if(mapa->jogador.altura_relativa > 3) { // caso altura da queda maior que 3
+            jog_diminui_vida(&(mapa->jogador)); // diminui vida do jogador
+        }
+
+        mapa->jogador.altura_relativa = 0; // ao cair, reseta a altura relativa do jogador para 1
+    }
+
     if((aux == ' ') || (aux == 'C') || (aux == '1') || (aux == '2')){
         if(aux == 'C'){
             mapa->bau = 1;
@@ -15,6 +27,8 @@ void gravidade(Mapa *mapa){
         mapa->mapa[mapa->jogador.localizacao.linha + 1][mapa->jogador.localizacao.coluna] = 'D';
         mapa->mapa[mapa->jogador.localizacao.linha][mapa->jogador.localizacao.coluna] = mapa->deletado;
         mapa->deletado = aux;
-    }
 
+        //printf("\n\naltura: %d     vidas: %d", mapa->jogador.altura_relativa, mapa->jogador.vidas);
+        mapa->jogador.altura_relativa += 1; // altura do jog em relação ao chao aumenta em 1;; representa um bloco de altura de queda
+    }
 }
