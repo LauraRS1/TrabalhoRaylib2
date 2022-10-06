d67fb3eeee35b4a1f20a43b7efbe564bf7a93e71
Thiago Arend <thiago.arend@gmail.com>
2022-10-06 19:53:58 -0300

    documentação camera.c (correção)

--- TrabalhoRaylib2/camera.c
+++ TrabalhoRaylib2/camera.c
@@ -1,6 +1,13 @@
 #include "coordenadas.h"
 #include "raylib.h"
 #include "desenha.h"
+
+/*
+    camera_atualiza:
+    Atualiza a câmera do jogador.
+    @param Camera2D *camera: ponteiro para a câmera do jogador.
+    @param Localizacao localizacao: a localização a ser apontada pela câmera do jogador.
+*/
 void camera_atualiza(Camera2D *camera, Localizacao localizacao){
     camera->target.x = 0 + localizacao.coluna * QUADRADO_LARGURA;
     camera->target.y = 0 + localizacao.linha * QUADRADO_ALTURA;
