d6688a2c422ef73ba8eefbf24c38225314b731e0
Thiago Arend <thiago.arend@gmail.com>
2022-10-06 20:25:36 -0300

    documentação...

--- TrabalhoRaylib2/desenha.c
+++ TrabalhoRaylib2/desenha.c
@@ -5,6 +5,7 @@
 
 
 void desenha_ranking(Ranking vetor[], Texture2D imagens[]){
+
     int i, j;
     int i_vezes=10, j_vezes=20;
     char numeros[TAM_VETOR][TAM_NOME]={0};
@@ -30,27 +31,34 @@ void desenha_ranking(Ranking vetor[], Texture2D imagens[]){
             DrawTexture(imagens[9], (0 + QUADRADO_LARGURA *j), (0 + QUADRADO_ALTURA * i), WHITE);
 
     DrawTexture(imagens[11], MEIO-250, ESPACO-30, WHITE);
-
-
-
 }
+
+
 void desenha_nvingame(){
+
     DrawText("Deseja iniciar um novo Jogo?", (LARGURA-MeasureText("Deseja iniciar um novo jogo?",TAMANHO_FONTE*2))/2, ALTURA/2 - 50, TAMANHO_FONTE*2, WHITE);
     DrawText("[S]", (LARGURA-MeasureText("[S]",TAMANHO_FONTE*2))/2 - 50, ALTURA/2 + 50, TAMANHO_FONTE*2, BLUE);
     DrawText("[N]", (LARGURA-MeasureText("[N]",TAMANHO_FONTE*2))/2 + 50, ALTURA/2 + 50, TAMANHO_FONTE*2, WHITE);
 }
+
+
 void desenha_retorna_menu(){
+
     DrawText("Deseja retornar ao Menu?", (LARGURA-MeasureText("Deseja retornar ao Menu?",TAMANHO_FONTE*2))/2, ALTURA/2 - 50, TAMANHO_FONTE*2, WHITE);
     DrawText("[S]", (LARGURA-MeasureText("[S]",TAMANHO_FONTE*2))/2 - 50, ALTURA/2 + 50, TAMANHO_FONTE*2, BLUE);
     DrawText("[N]", (LARGURA-MeasureText("[N]",TAMANHO_FONTE*2))/2 + 50, ALTURA/2 + 50, TAMANHO_FONTE*2, WHITE);
 }
 
+
 void desenha_load(){
+
     DrawText("Carregando...", (LARGURA-MeasureText("Carregando...",TAMANHO_FONTE*3))/2, ALTURA/2 - 50, TAMANHO_FONTE*3, WHITE);
 
 }
 
+
 void desenha_gameover(Texture2D imagens[], int pontuacao, int ultimo_lugar, char nome_ranking[]){
+
     DrawText("GAME OVER", (LARGURA-MeasureText("GAME OVER",TAMANHO_FONTE*3))/2, ALTURA/2 - 50, TAMANHO_FONTE*3, WHITE);
 
     int i, j;
@@ -63,12 +71,16 @@ void desenha_gameover(Texture2D imagens[], int pontuacao, int ultimo_lugar, char
     DrawTexture(imagens[11], MEIO-33, 330, WHITE);
 
     if(pontuacao > ultimo_lugar){
+
         DrawText("Otima pontuacao! digite seu nome:", (LARGURA-MeasureText("Ótima pontuação! digite seu nome:",TAMANHO_FONTE))/2, ALTURA/2 + 100, TAMANHO_FONTE, WHITE);
     }
 
 
 }
+
+
 void desenha_fim(Texture2D imagens[], int pontuacao, int ultimo_lugar, char nome_ranking[]){
+
     DrawText("FIM", (LARGURA-MeasureText("FIM!",TAMANHO_FONTE*3))/2, ALTURA/2 - 50, TAMANHO_FONTE*3, WHITE);
 
     int i, j;
@@ -85,17 +97,23 @@ void desenha_fim(Texture2D imagens[], int pontuacao, int ultimo_lugar, char nome
 
 }
 
+
 void desenha_proximo(){
+
     DrawText("Passou de Nivel", (LARGURA-MeasureText("Passou de Nivel",TAMANHO_FONTE*3))/2, ALTURA/2 - 50, TAMANHO_FONTE*3, WHITE);
 
 }
 
+
 void desenha_msg_checkpoint(){
+
     DrawText("Checkpoint...", BASE_X + (TAMANHO_CHECKPOINT / 2), ALTURA - (TAMANHO_CHECKPOINT * 1.625), TAMANHO_FONTE, WHITE);
 
 }
 
+
 void desenha_menu(int selecionada) {
+
     char menu[OPCOES][TAMANHO_TEXTO] = {"Novo Jogo", "Carregar Jogo","Ranking de Pontos", "Sair"};
     char mudado[TAMANHO_TEXTO];  // string para guardar o texto modificado
     // desenha o texto de acordo com o parametro dado
@@ -140,7 +158,9 @@ void desenha_menu(int selecionada) {
       }
 }
 
+
 void desenha_hud(Mapa *mapa, Texture2D imagens[]) {
+
     char saida_pontos[TAMANHO_TEXTO], saida_vida[TAMANHO_TEXTO], saida_fase[TAMANHO_TEXTO], saida_chave[TAMANHO_TEXTO];
     int x=0, y=0;
 
@@ -180,6 +200,7 @@ void desenha_hud(Mapa *mapa, Texture2D imagens[]) {
     DrawText(mapa->jogador.ultimo_item, x, y, TAMANHO_HUD, DARKBLUE);
 
 }
+
 
 void desenha_nivel(Mapa *mapa, Texture2D imagens[]){
 
@@ -226,9 +247,6 @@ void desenha_nivel(Mapa *mapa, Texture2D imagens[]){
                 //parede do fundo
                 if(mapa->mapa[i][j] == ' ')
                     DrawTexture(imagens[9], (0 + QUADRADO_LARGURA *j), (0 + QUADRADO_ALTURA * i), WHITE);
-
                 }
-
-
         }
  }
